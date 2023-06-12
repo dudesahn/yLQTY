1fb2e7f3f7054e70d15720b6c1792c8c189a574a
dudesahn <dudesahn@pm.me>
2023-06-12 12:03:13 -0400

    chore: remove extra address casting

--- contracts/StrategyLQTYStaker.sol
+++ contracts/StrategyLQTYStaker.sol
@@ -442,7 +442,7 @@ contract StrategyLQTYStaker is BaseStrategy {
             0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf
         ).latestRoundData(
                 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
-                address(0x0000000000000000000000000000000000000348) // USD, returns 1e8
+                0x0000000000000000000000000000000000000348 // USD, returns 1e8
             );
 
         uint256 claimableLusd = lqtyStaking.getPendingLUSDGain(address(this));
